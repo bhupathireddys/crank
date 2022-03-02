1dfbd72ccf481cb33b2e35136bfaa3b48b33f089
unknown <bhupathireddys@gmail.com>
2022-03-02 05:26:30 +0000

    commited dv2

---
+++ dotnetinstall.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+apt-get update
+apt-get install -y --no-install-recommends \
+        git \
+        curl \
+        wget
+
+cd ~
+wget https://download.visualstudio.microsoft.com/download/pr/807f9d72-4940-4b1a-aa4a-8dbb0f73f5d7/cb666c22a87bf9413f29615e0ba94500/dotnet-sdk
+-6.0.200-linux-x64.tar.gz
+
+mkdir -p $HOME/dotnet && tar zxf dotnet-sdk-6.0.200-linux-x64.tar.gz -C $HOME/dotnet
+
+export DOTNET_ROOT=$HOME/dotnet
+export PATH=$PATH:$HOME/dotnet
+
+dotnet tool install -g Microsoft.Crank.Controller --version "0.2.0-*"
+dotnet tool install -g Microsoft.Crank.Agent --version "0.2.0-*"
+
+cat << \EOF >> ~/.bash_profile
+# Add .NET Core SDK tools
+export DOTNET_ROOT=$HOME/dotnet
+export PATH=$PATH:$HOME/dotnet
+export PATH="$PATH:/root/.dotnet/tools"
+EOF
+
+echo " Dotnet and tools installed successfully"
